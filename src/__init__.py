a927f0249e95f2d4f0578ea4827b503fcb817803
SatvikR <49799352+SatvikR@users.noreply.github.com>
2020-08-28 10:16:25 -0700

    added icons and changed directory structure

--- src/__init__.py
+++ src/__init__.py
@@ -2,4 +2,5 @@ from flask import Flask
 app = Flask(__name__)
 app.secret_key = 'SECRET_KEY_HERE'
 
+# App decleration must be first
 import src.views
--- src/views/index.py
+++ src/views/index.py
@@ -4,4 +4,4 @@ from flask import render_template
 
 @app.route('/')
 def index():
-    return render_template('index.html', logged_in=False, username='test_user')
+    return render_template('index.html')
