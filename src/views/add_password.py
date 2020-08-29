8d03d916617660038e86a6f3a7a90d2d325c6e07
SatvikR <49799352+SatvikR@users.noreply.github.com>
2020-08-28 23:54:03 -0700

    Release 1

--- src/views/add_password.py
+++ src/views/add_password.py
@@ -1,6 +1,35 @@
 from src import app
-from flask import render_template
+from src.db import query
+from flask import (
+    render_template,
+    request,
+    session,
+    redirect,
+    url_for
+)
+from base64 import b64encode
 
-@app.route('/add_password')
+@app.route('/add_password', methods=['GET', 'POST'])
 def add_password():
-    return render_template('add_password.html')
+    if request.method == 'POST':
+        website = request.form['website']
+        password = request.form['password']
+
+        hashed_password = b64encode(password.encode()).decode()
+
+        target_user = query(f"""SELECT id
+                        FROM users
+                        WHERE username='{session['username']}'""")
+
+        uid, = target_user[0] # Destructuring query
+
+        query(f"""INSERT INTO passwords
+                (uid, website, password)
+                VALUES ({uid}, '{website}', '{hashed_password}')""")
+        
+        return redirect(url_for('your_passwords'))
+    else:
+        if 'username' not in session:
+            return redirect(url_for('login'))
+        
+        return render_template('add_password.html')
